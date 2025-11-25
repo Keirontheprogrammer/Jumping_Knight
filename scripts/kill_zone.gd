4889977589d848f83ecb85bd671d5e1dbc6297fe
Tamandani Nsiku <sebastiangooddays@gmail.com>
2025-11-25 11:12:35 -0800

    level two scene sketch

--- scripts/kill_zone.gd
+++ scripts/kill_zone.gd
@@ -7,7 +7,7 @@ func _on_body_entered(body) -> void:
 	print("DEATH comes in Three");
 	timer.start();
 	
-
-
 func _on_timer_timeout() -> void:
-	get_tree().reload_current_scene();
+	#get_tree().reload_current_scene();
+	
+	get_tree().change_scene_to_file("res://scenes/level_2.tscn")
