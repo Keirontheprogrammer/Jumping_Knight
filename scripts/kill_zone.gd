57c09a90d5441c9ee619b779cdd0b9c03552ad12
Tamandani Nsiku <sebastiangooddays@gmail.com>
2025-11-25 14:05:16 -0800

    working portsl

--- scripts/kill_zone.gd
+++ scripts/kill_zone.gd
@@ -8,6 +8,6 @@ func _on_body_entered(body) -> void:
 	timer.start();
 	
 func _on_timer_timeout() -> void:
-	#get_tree().reload_current_scene();
+	get_tree().reload_current_scene();
 	
-	get_tree().change_scene_to_file("res://scenes/level_2.tscn")
+	#get_tree().change_scene_to_file("res://scenes/level_2.tscn")
