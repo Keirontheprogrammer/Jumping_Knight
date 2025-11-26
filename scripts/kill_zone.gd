0c0cc610840b410d97e4a76112bf5094e90ddb0d
Tamandani Nsiku <sebastiangooddays@gmail.com>
2025-11-26 07:41:54 -0800

    enemies from hell

--- scripts/kill_zone.gd
+++ scripts/kill_zone.gd
@@ -3,11 +3,15 @@ extends Area2D
 @onready var timer: Timer = $Timer
  
 
-func _on_body_entered(body) -> void:
+func _on_body_entered(body: Node2D) -> void:
 	print("DEATH comes in Three");
+	Engine.time_scale = 0.3;
+	
+	var collision= body.get_node("CollisionShape2D");
+	collision.queue_free();  
 	timer.start();
 	
 func _on_timer_timeout() -> void:
+	Engine.time_scale = 1;
 	get_tree().reload_current_scene();
 	
-	#get_tree().change_scene_to_file("res://scenes/level_2.tscn")
