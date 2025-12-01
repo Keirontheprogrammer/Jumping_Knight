6285606f963f5f65af449046a116ad24e89da900
Tamandani Nsiku <sebastiangooddays@gmail.com>
2025-12-01 02:20:52 -0800

    sound effects

--- scripts/coin.gd
+++ scripts/coin.gd
@@ -1,8 +1,9 @@
 extends Area2D
 @onready var manager: Node = %manager
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
 
 
 # Called when the node enters the scene tree for the first time.
 func _on_body_entered(body) -> void:
 	manager.add_score()
-	queue_free()
+	animation_player.play("pickup_nimation")
--- scripts/kill_zone.gd
+++ scripts/kill_zone.gd
@@ -2,9 +2,11 @@ extends Area2D
 
 @onready var timer: Timer = $Timer
 @onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
 
 
 func _on_body_entered(body: Node2D) -> void:
+	animation_player.play("hurt");
 	print("DEATH comes in Three")
 
 	# Check if the body is the player
