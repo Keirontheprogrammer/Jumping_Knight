fe98acd7378eab1c4b8da3d260e0875db0d73a57
Tamandani Nsiku <sebastiangooddays@gmail.com>
2025-12-01 13:32:13 -0800

    did not know what is was doing

--- scripts/kill_zone.gd
+++ scripts/kill_zone.gd
@@ -3,12 +3,14 @@ extends Area2D
 @onready var timer: Timer = $Timer
 @onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
+@onready var player: CharacterBody2D = $"../../../player"
 
 
 func _on_body_entered(body: Node2D) -> void:
 	animation_player.play("hurt");
 	print("DEATH comes in Three")
-
+	
+	
 	# Check if the body is the player
 	if body is CharacterBody2D:
 		
